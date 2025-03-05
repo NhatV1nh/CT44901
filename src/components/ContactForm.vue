7baeab14c22750ccaf0acedae9b7ff186bcebf86
NhatV1nh <vinhb2014632@student.ctu.edu.vn>
2025-03-06 01:07:25 +0700

    Tao trang cap nhat lien he

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -23,80 +23,78 @@
       <div class="form-group form-check">
         <input name="favorite" type="checkbox" class="form-check-input" v-model="contactLocal.favorite" />
         <label for="favorite" class="form-check-label">
-          <strong>Liên hệ yêu thích</strong>
-        </label>
+          <strong>Liên hệ yêu thích</strong></label>
       </div>
       <div class="form-group">
         <button class="btn btn-primary">Lưu</button>
-        <button v-if="contactLocal.id" type="button" class="ml-2 btn btn-danger" @click="deleteContact">
+        <button v-if="contactLocal._id" type="button" class="ml-2 btn btn-danger" @click="deleteContact">
           Xóa
         </button>
-        <button type="button" class="ml-2 btn btn-danger" @click="Cancel">
-          Thoát
-        </button>
       </div>
+  
     </Form>
   </template>
   
   <script>
-  import * as yup from "yup";
- import { Form, Field, ErrorMessage } from "vee-validate";
-
-export default {
-  components: {
-    Form,
-    Field,
-    ErrorMessage,
-  },
-  emits: ["submit:contact", "delete:contact"],
-  props: {
-    contact: { type: Object, required: true }
-  },
-  data() {
-    const contactFormSchema = yup.object().shape({
-      name: yup
-        .string()
-        .required("Tên phải có giá trị.")
-        .min(2, "Tên phải ít nhất 2 ký tự.")
-        .max(50, "Tên có nhiều nhất 50 ký tự."),
-      email: yup
-        .string()
-        .email("E-mail không đúng.")
-        .max(50, "E-mail tối đa 50 ký tự."),
-      address: yup.string().max(100, "Địa chỉ tối đa 100 ký tự."),
-      phone: yup
-        .string()
-        .matches(/(09|03|07|08|05)+([0-9]{8})\b/, "Số điện thoại không hợp lệ."),
-    });
-
-    return {
-      // Chúng ta sẽ không muốn hiệu chỉnh props, nên tạo biến cục bộ
-      // contactLocal để liên kết với các input trên form
-      contactLocal: this.contact,
-      contactFormSchema,
-    };
-  },
-  methods: {
-    submitContact() {
-      this.$emit("submit:contact", this.contactLocal);
+  
+  import * as yup from "yup"
+  import { Form, Field, ErrorMessage } from "vee-validate"
+  
+  export default {
+    components: {
+      Form,
+      Field,
+      ErrorMessage,
     },
-    deleteContact() {
-      this.$emit("delete:contact", this.contactLocal.id);
+  
+    emits: ["submit:contact", "delete:contact"],
+  
+    props: {
+      contact: { type: Object, required: true }
     },
-    Cancel() {
-      const reply = window.confirm("You have unsaved changes! Do you want to leave?");
-      if (!reply) {
-        return false; // stay on the page if user clicks 'Cancel'
-      } else {
-        this.$router.push({ name: "contactbook" });
-      }
+  
+    data() {
+      const contactFormSchema = yup.object().shape({
+        name: yup
+          .string()
+          .required("Tên phải có giá trị.")
+          .min(2, "Tên phải ít nhất 2 ký tự.")
+          .max(50, "Tên có nhiều nhất 50 ký tự."),
+        email: yup
+          .string()
+          .email("E-mail không đúng.")
+          .max(50, "E-mail tối đa 50 ký tự."),
+        address: yup.string().max(100, "Địa chỉ tối đa 100 ký tự."),
+        phone: yup
+          .string()
+          .matches(
+            /((09|03|07|08|05)+([0-9]{8})\b)/g,
+            "Số điện thoại không hợp lệ."
+          ),
+      });
+      return {
+        // Chúng ta sẽ không muốn hiệu chỉnh props, nên tạo biến cục bộ
+        // contactLocal để liên kết với các input trên form
+        contactLocal: this.contact,
+        contactFormSchema,
+      };
     },
-  },
-};
-</script>
-
-<style scoped>
-@import "@/assets/form.css";
-</style>
-
+    methods: {
+      submitContact() {
+        this.$emit("submit:contact", this.contactLocal);
+        this.$router.push({ path: '/'})
+      },
+      deleteContact() {
+        this.$emit("delete:contact", this.contactLocal.id);
+      },
+  
+    },
+  };
+  </script>
+  
+  <style scoped>
+  
+  @import "@/assets/main.css";
+  
+  </style>
   
--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -44,8 +44,7 @@
           :to="{
             name: 'contact.edit',
             params: { id: activeContact._id },
-          }"
-        >
+          }">
           <span class="mt-2 badge badge-warning">
             <i class="fas fa-edit"></i> Hiệu chỉnh</span
           >
